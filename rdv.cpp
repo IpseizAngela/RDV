6e8fb802621d1d939affa1d13d68b8aaeb7c61de
Angela Ipseiz <angela.ipseiz8@etu.univ-lorraine.fr>
2020-01-29 21:15:45 +0100

    création d'une image ppm sans stb

--- rdv.cpp
+++ rdv.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <stdlib.h>
+#include <string.h>
 #include <vector>
 #include "data.hpp"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
 
-#define WIDTH 200
-#define HEIGHT 200
+#define WIDTH 10
+#define HEIGHT 10
+
+void writepgm(const unsigned char* image, const char* filename){
+    int i;
+    FILE* F = fopen(filename,"w");
+    if (F) {
+        fprintf(F, "P3\n%d %d\n255\n", WIDTH, HEIGHT);
+        unsigned char r, v, b;
+        for (i = 0; i < WIDTH * HEIGHT; i++) {
+            fprintf(F, "%d %d %d ", image[i*3 + 0], image[i*3 + 1], image[i*3 + 2]);
+        }
+        fclose(F);
+    }
+}
 
 void render() {
     Color c = {127,127,127};
@@ -17,11 +28,10 @@ void render() {
             pixmap[i*3+j] = (unsigned char)127;
         }
     }
-    stbi_write_jpg("out.jpg", WIDTH, HEIGHT, 3, pixmap.data(), 100);
+    writepgm(pixmap.data(), "test.ppm");
 }
 
 int main() {
-    //std::cout << "Hello, World!" << std::endl;
     render();
     return 0;
 }
