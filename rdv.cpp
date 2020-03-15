7e2087f7a5a520f2fd55eee5a2908c7ef36ba92b
IpseizAngela <31900083+IpseizAngela@users.noreply.github.com>
2020-03-15 11:19:14 +0100

    anaglyphe

--- rdv.cpp
+++ rdv.cpp
@@ -9,11 +9,13 @@
 #define WIDTH 800
 #define HEIGHT 800
 #define PROF 100
+#define DELTA 15
 
 
 Point dir_lum = {0, 0, -1};
-Point camera(0,0,3);
+Point cam(0,0,3);
 Color col_diablo;
+
 
 using namespace std;
 
@@ -177,84 +179,78 @@ Pointi pointToPointi(Point p, float d) {
 }
 
 
-void render(Model m) {
+std::vector<Color> draw(Point camera, Model m) {
 	int NB_PT = 3;
-	int TAILLE = WIDTH*HEIGHT;
-	float dcam = .2;
-    
-    Color blanc = {255,255 ,255, 0};
-    Color noir = {0, 0, 0, 0};
-	col_diablo = blanc;
+	int TAILLE = WIDTH*HEIGHT + DELTA;
+	Color noir = {0, 0, 0, 0};
 	Matrix proj = Matrix::identity(4);
 	Matrix view = viewport(WIDTH/8, HEIGHT/8, WIDTH*3/4, HEIGHT*3/4);
-
-	Point eye = {camera.x, camera.y, 0};
-	Point center = {0, 0, 10};
-	Point up = {eye.x,eye.y+camera.z,eye.z};
-	Matrix look = lookat(eye, center, up);
 	proj[3][2] = -1.f/camera.z;
 	
-	Point eyeR = {camera.x - (dcam/2.f), camera.y, 0};
-	Point eyeB = {camera.x + (dcam/2.f), camera.y, 0};
-	Matrix lookR = lookat(eyeR, center, up);
-	Matrix lookB = lookat(eyeB, center, up);
+	Point eye = {camera.x, camera.y, 0};
+	Point center = {0, 0, 0};
+	Point up = {camera.x,camera.y+3,camera.z};
+	Matrix look = lookat(camera, center, up);
 	
-	
-	std::vector<Color> pixels(TAILLE),pixelsR(TAILLE),pixelsB(TAILLE);
-	std::vector<float> zBufferR(TAILLE), zBufferB(TAILLE);
+	std::vector<Color> pixels(TAILLE);
+	std::vector<float> zBuffer(TAILLE);
 	
 	std::vector<float> intensites(NB_PT);
 	std::vector<Point> points_modele(NB_PT);
-	std::vector<Pointi> points_ecranR(NB_PT), points_ecranB(NB_PT);
+	std::vector<Pointi> points_ecran(NB_PT);
 	
     for (size_t i = 0; i < TAILLE; ++i) {
-        pixelsR[i] = noir;
-        pixelsB[i] = noir;
-		zBufferR[i] = -TAILLE*1000;
-        zBufferB[i] = -TAILLE*1000;
+        pixels[i] = noir;
+		zBuffer[i] = -TAILLE*1000;
     }
-	
-	int delta = 15; 	
+		
 	for (int nface = 0; nface < m.nbfaces(); nface++) {
 		for (int i = 0; i < NB_PT; i++) {
 			points_modele[i] = m.point(m.vert(nface, i));
-			// points_ecranR[i] = mat2pt(view*proj*lookR*pt2mat(points_modele[i]));
-			points_ecranB[i] = mat2pt(view*proj*lookB*pt2mat(points_modele[i]));
-			points_ecranR[i] = mat2pt(view*proj*look*pt2mat(points_modele[i]));
+			points_ecran[i] = mat2pt(view*proj*look*pt2mat(points_modele[i]));
 			intensites[i] = -(m.normal(nface, i) * dir_lum);
 		}
-		
-		trianglePlein(points_ecranR, intensites, pixelsR, zBufferR);
-		trianglePlein(points_ecranB, intensites, pixelsB, zBufferB);
+		trianglePlein(points_ecran, intensites, pixels, zBuffer);
 	}
-    
-	for (size_t i = 0; i < WIDTH*HEIGHT; i++) {
-        Color coulfinale = {(pixelsR[i].r+pixelsR[i].g+pixelsR[i].b)/3, 0, (pixelsB[i].r+pixelsB[i].g+pixelsB[i].b)/3, 0};
-        pixels[i] = coulfinale;
-    }
-    
-    std::vector<unsigned char> pixmap(WIDTH*HEIGHT*3);
-    for (size_t i = 0; i < HEIGHT*WIDTH; ++i) {
-        for (size_t j = 0; j<3; j++) {
-            pixmap[i*3+j] = (unsigned char) pixelsR[i][j];
+	
+	return pixels;
+}
+
+
+void render(Model m) {
+	int TAILLE = WIDTH*HEIGHT;
+	float dcam = .1;
+   
+	col_diablo = {255,255 ,255, 0};
+	
+	Point camR = {cam.x + (dcam/2.f), cam.y, cam.z};
+	Point camB = {cam.x - (dcam/2.f), cam.y, cam.z};
+	
+	std::vector<Color> pixelsR = draw(camR, m);
+	std::vector<Color> pixelsB = draw(camB, m);
+
+	std::vector<unsigned char> pixmap(WIDTH*HEIGHT*3);
+    for (size_t j = 0; j<HEIGHT; j++) {
+        for (size_t i = 0; i<WIDTH; i++) {
+            Color R = pixelsR[i+DELTA+j*WIDTH];
+            Color B = pixelsB[i+j*WIDTH];
+			Color finale = {(R.r+R.g+R.b)/3.f, 0, (B.r+B.g+B.b)/3.f, 0};
+			
+			for (size_t k = 0; k<3; k++) {
+				pixmap[(j*WIDTH+i)*3+k] = (unsigned char) finale[k];
+			}
         }
     }
+	
     writeppm(pixmap.data(), "test1.ppm");
 }
 
 
 int main(int argc, char** argv) {
-    cout << "Usage: " << argv[0] << " model.obj tangentnormals.jpg diffuse.jpg specular.jpg" << endl;
     string file_obj ("../lib/obj/diablo3_pose/diablo3_pose.obj");
     string file_nm  ("../lib/obj/diablo3_pose/diablo3_pose_nm_tangent.tga");
     string file_diff("../lib/obj/diablo3_pose/diablo3_pose_diffuse.tga");
     string file_spec("../lib/obj/diablo3_pose/diablo3_pose_spec.tga");
-    if (5==argc) {
-        file_obj  = std::string(argv[1]);
-        file_nm   = std::string(argv[2]);
-        file_diff = std::string(argv[3]);
-        file_spec = std::string(argv[4]);
-    }
     Model model(file_obj.c_str());
     cout << "Number of points in the model: " << model.nbvertex() << " Number of faces in the model " << model.nbfaces() << endl;
     render(model);
